56cbabb80593819cd8aa1da7d0462ae32e307229
JsMendoza13 <jsmendoza1312@gmail.com>
2023-02-07 20:28:43 -0500

    Comienzo de proyecto martes 07 feb

--- final-react-app/src/App.jsx
+++ final-react-app/src/App.jsx
@@ -1,32 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Error404 from "./components/helpers/Error404"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home</h1>,
+    errorElement: <Error404 />
+  }
+])
 
+
+function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </div>
   )
 }
