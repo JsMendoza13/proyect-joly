4673a1aae1aeb970ba5907cfeb67cc721759b810
santiago a <santiagoalvarezporras@gmail.com>
2023-02-10 05:27:17 -0500

    cree el formulario y la tabla de productos

--- final-react-app/src/App.jsx
+++ final-react-app/src/App.jsx
@@ -2,11 +2,17 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Error404 from "./components/helpers/Error404"
 import Header from './components/pages/Header'
 import Main from './components/pages/Main'
+import Crear from './components/productos/Crear'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    errorElement: <Error404 />
+  },
+  {
+    path: '/crear',
+    element: <Crear />,
     errorElement: <Error404 />
   }
 ])
--- final-react-app/src/components/productos/Crear.jsx
+++ final-react-app/src/components/productos/Crear.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Formularios from '../../css/Formularios.css'
 
 const Crear = () => {
   return (
-    <div>Crear</div>
+    <main>
+       <section className='formulario'>
+        <form id="formulario" method="post"  name="form" action="">
+
+            
+            <input type="text" placeholder="texto" name="texto" />
+
+            <input type="text" placeholder="texto" name="texto" />
+
+            <input type="email" placeholder="texto" name="texto" />  
+            
+            <input type="text" placeholder="texto" name="texto" />
+
+            <input type="text" placeholder="texto" name="texto" />
+
+            <input type="submit" value="Agregar" className='boton' name="add"/>
+        </form>
+    </section>
+    </main>
+   
   )
 }
 
