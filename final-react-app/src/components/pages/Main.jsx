7821ec504a7a3b34a9a7c5292c8139a7ac54d473
JsMendoza13 <jsmendoza1312@gmail.com>
2023-02-11 11:13:00 -0500

    Se arreglan algunos detalles del main

--- final-react-app/src/components/pages/Main.jsx
+++ final-react-app/src/components/pages/Main.jsx
@@ -23,8 +23,10 @@ const Main = () => {
 
             </div>
                 {/*  boton contactame */}
-         <div className='md:ml-[70%] m-5 '>
+         <div className='md:ml-[70%] m-5 mt-10 md:-mt-20 '>
+         <a target="_blank" href="https://github.com/JsMendoza13/proyect-joly"> 
           <button className='BtnImg text-quaternary'>Contáctanos</button>
+          </a>
          </div>
               
         </section>
@@ -41,7 +43,7 @@ const Main = () => {
         </section>
         </section>
                       {/* Se llama al Slider */}
-        <section> <Slider/> </section>
+        <section className='mt-60 md:mt-0'> <Slider/> </section>
 
               {/*  PARTE DE LA MISION - VISION */}
         <section className='h-[100vh] grid grid-cols-1 md:grid-cols-8 mt-40'>
@@ -49,7 +51,7 @@ const Main = () => {
           <section className='md:col-span-4 flex items-center justify-center p-8'>
           <div>
             <h1 className='font-bold m-1 ml-5 text-6xl text-secundary' >Mision</h1>
-            <img className='md:mt-10 mt-[7%] md:w-[45%] w-[80%] md:h-86 h-60 ' src={mision} />
+            <img className='md:mt-10 mt-[7%] md:w-[45%] w-[70%] md:h-86 h-55 ' src={mision} />
 
           <p className='relative bg-emerald-900 shadow-2xl rounded-tr-3xl rounded-br-lg md:p-3 p-5 w-full md:w-[50%] flex flex-col md:ml-[30%] ml-[0%] md:-translate-x-[-40%] md:-mt-[23%] mt-[5%]  h-[100%] text-quaternary'>Guacamole y Hummus es un proyecto innovador que ofrece deliciosos platillos con la misión de llevar el sabor de nuestra region a cualquier cocina. 
           Ademas es una empresa que se esfuerza por crear un impacto significativo en el mundo. Como esfuerzo empresarial, valoramos la creación de un ambiente de trabajo colaborativo y empoderado. Nuestro objetivo es empoderar a las personas a través de nuestros productos y brindarles la oportunidad de aprender nuevas habilidades mientras se divierten.</p></div>
@@ -58,7 +60,7 @@ const Main = () => {
         <section className='md:col-span-4 flex items-center justify-center p-8'>
           <div>
             <h1 className='font-bold m-1 ml-5 text-6xl text-secundary md:float-right'>Vision</h1>
-            <img className='md:mt-10 mt-[20%] md:w-[55%] w-[80%]  h-80  -rotate-3  ' src={vision} />
+            <img className='md:mt-10 mt-[20%] md:w-[55%] w-[78%]  md:h-70 h-[80%]  -rotate-3  ' src={vision} />
 
             <p className='relative bg-emerald-900 shadow-2xl rounded-tr-3xl rounded-br-lg xl:p-5 p-5 mt-[2%]  h-[100%] text-quaternary'>Creemos en una visión empresarial que está guiada por la pasión de crear productos sorprendentes mientras promovemos la calidad y la nutrición saludable.
             A través de nuestra innovación, hacemos que la alimentación saludable sea más fácil, más asequible, más accesible y, lo que es más importante, ¡deliciosa!
