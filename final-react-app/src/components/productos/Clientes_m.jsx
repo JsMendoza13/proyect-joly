4eb10404dbc41858c7d5ae064070eaa1bfe6828d
JsMendoza13 <jsmendoza1312@gmail.com>
2023-02-11 18:30:10 -0500

    creacion crearCliente

---
+++ final-react-app/src/components/productos/Clientes_m.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CrearCli from './CrearCli'
+
+const Clientes_m = () => {
+  return (
+    <main>
+      <CrearCli /> 
+    </main>
+  )
+}
+
+export default Clientes_m
