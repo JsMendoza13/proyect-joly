01c111b73991029753722f3bbbbb0656256abe83
JsMendoza13 <jsmendoza1312@gmail.com>
2023-02-11 20:55:11 -0500

    Iniciando creacion de proveedores

--- final-react-app/src/App.jsx
+++ final-react-app/src/App.jsx
@@ -1,4 +1,3 @@
-/* import {createBrowserRouter, RouterProvider} from 'react-router-dom' */
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Error404 from "/src/components/helpers/Error404"
 import Footer from '/src/components/pages/Footer'
@@ -6,21 +5,8 @@ import Header from '/src/components/pages/Header'
 import Main from '/src/components/pages/Main'
 import Productos_m from './components/productos/Productos_m'
 import Clientes_m from './components/clientes/Clientes_m';
+import Proveedores_m from './components/proveedores/Proveedores_m';
 import Editar from './components/productos/Editar';
-
-
-/* const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main />,
-    errorElement: <Error404 />
-  },
-  {
-    path: '/productos',
-    element: <Productos_m />,
-    errorElement: <Error404 />
-  }
- ]) */
 
 
 function App() {
@@ -30,12 +16,14 @@ function App() {
        
        <Routes>
        <Route path="/" element={<Main />}/> 
-       <Route path="/productos" element={<Productos_m/>}/> 
-       <Route path="/clientes" element={<Clientes_m/>}/>     
-       <Route path="/editar/:id" element={<Editar/>}/>  
+       <Route path="/productos" element={<Listado/>}/> 
+       <Route path="/clientes" element={<Clientes_m/>}/>
+       <Route path="/proveedores" element={<Proveedores_m/>}/>    
+       <Route path="/editar/:id" element={<Editar/>}/> 
+       <Route path="/crear" element={<Crear/>}/>   
+       
        </Routes>
-        
-    {/*   <RouterProvider router={router} /> */}
+
       <Footer/>
             </Router> 
   )
