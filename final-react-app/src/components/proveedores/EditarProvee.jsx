30ff55ef865fa40272682a149636ea2085bd6e62
JsMendoza13 <jsmendoza1312@gmail.com>
2023-02-12 14:37:59 -0500

    Agrega tamaño a la tabla proveedores

--- final-react-app/src/components/proveedores/EditarProvee.jsx
+++ final-react-app/src/components/proveedores/EditarProvee.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 const urlproveedores = "http://localhost:3100/proveedores";
-import Formularios from "../../css/Formularios.css"
+import Formularios from "/src/css/Formularios.css";
 
 export const EditarProvee = () => {
   const [nit, setNit] = useState("");
-    const [nombre, setNombre] = useState("");
-    const [nombreRepresentante, setNombreRepresentante] = useState("");
-    const [telefono, setTelefono] = useState("");
-    const [correo, setCorreo] = useState("");
-    const [direccion, setDireccion] = useState("");
-    const [logo, setLogo] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [nombreRepresentante, setNombreRepresentante] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [logo, setLogo] = useState("");
   const navigate = useNavigate();
-  const {id} = useParams()
+  const { id } = useParams();
 
   const actualizarProveedores = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const proveedores = {
       nit,
       nombre,
@@ -29,91 +29,94 @@ export const EditarProvee = () => {
       logo,
     };
     await axios.put(`${urlproveedores}/${id}`, proveedores);
-    navigate('/proveedores')
+    navigate("/proveedores");
   };
 
   const listarProveedores = async () => {
-    const res = await axios.get(`${urlproveedores}/${id}`)
-    setNit(res.data.nit)
-    setNombre(res.data.nombre)
-    setNombreRepresentante(res.data.nombreRepresentante)
-    setTelefono(res.data.telefono)
-    setCorreo(res.data.correo)
-    setDireccion(res.data.direccion)
-    setLogo(res.data.logo)
-  }
+    const res = await axios.get(`${urlproveedores}/${id}`);
+    setNit(res.data.nit);
+    setNombre(res.data.nombre);
+    setNombreRepresentante(res.data.nombreRepresentante);
+    setTelefono(res.data.telefono);
+    setCorreo(res.data.correo);
+    setDireccion(res.data.direccion);
+    setLogo(res.data.logo);
+  };
 
-  useEffect(()=> {
-    listarProveedores()
-  }, [])
+  useEffect(() => {
+    listarProveedores();
+  }, []);
 
   return (
     <main className="-mb-[170%] md:mb-[0%]">
-       <h2 className='text-5xl text-center text-primary py-5' >Editar información de proveedores.</h2>
-      <section className='formulario'>
-    <form>
-            
-    <input 
-              onChange={(e) => setNit(e.target.value)}
-              value={nit}
-              placeholder="Nit"
-              type="text"
-            />
+      <h2 className="text-5xl text-center text-primary py-5">
+        Editar información de proveedores.
+      </h2>
+      <section className="formulario">
+        <form>
+          <input
+            onChange={(e) => setNit(e.target.value)}
+            value={nit}
+            placeholder="Nit"
+            type="text"
+          />
 
-            <input 
-               onChange={(e) => setNombre(e.target.value)}
-               value={nombre}
-               placeholder="Nombre"
-               type="text"
-            />
+          <input
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+            placeholder="Nombre"
+            type="text"
+          />
 
-            <input 
+          <input
             onChange={(e) => setNombreRepresentante(e.target.value)}
             value={nombreRepresentante}
             placeholder="Nombre del representante"
             type="text"
-            />  
-            
-            <input 
+          />
+
+          <input
             onChange={(e) => setTelefono(e.target.value)}
             value={telefono}
             placeholder="Telefono"
             type="text"
-            />
+          />
 
-            <input 
+          <input
             onChange={(e) => setCorreo(e.target.value)}
             value={correo}
             placeholder="Correo"
             type="text"
-            />
+          />
 
-            <input 
+          <input
             onChange={(e) => setDireccion(e.target.value)}
             value={direccion}
             placeholder="Direccion"
             type="text"
-            />
+          />
 
-            <input 
+          <input
             onChange={(e) => setLogo(e.target.value)}
             value={logo}
             placeholder="logo"
             type="text"
-            />
+          />
 
-           <Link onClick={actualizarProveedores} className='boton text-neutral-50 text-center'>Actualizar</Link>  
+          <Link
+            onClick={actualizarProveedores}
+            className="boton text-neutral-50 text-center"
+          >
+            Actualizar
+          </Link>
 
           <Link to={"/proveedores"} className="btnCancel">
             Cancelar
-          </Link>  
+          </Link>
+        </form>
+      </section>
+    </main>
+  );
+};
 
-   
-</form>
-</section>
-</main>
-  )
-}
-
-
-export default EditarProvee
+export default EditarProvee;
